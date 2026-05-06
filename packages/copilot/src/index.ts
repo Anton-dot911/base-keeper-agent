8bdc550bae011e9920cfe5940f5632aef6ed6634
Anton-dot911 <132948024+Anton-dot911@users.noreply.github.com>
2026-05-06 04:24:57 +0300

    Add Keeper Copilot public exports

---
+++ packages/copilot/src/index.ts
@@ -0,0 +1,7 @@
+export { buildCopilotSummary } from "./analyzer.js";
+export { explainExecutionBlocker, toCopilotTopSignal } from "./signal-explainer.js";
+export type {
+  CopilotRiskDistribution,
+  CopilotSummary,
+  CopilotTopSignal
+} from "./types.js";
