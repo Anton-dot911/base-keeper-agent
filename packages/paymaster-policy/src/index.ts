ad81a960f1d3e2bcfc7d63d3e0d7798bae7def9d
Anton-dot911 <132948024+Anton-dot911@users.noreply.github.com>
2026-05-06 07:28:16 +0300

    Export paymaster policy summary

--- packages/paymaster-policy/src/index.ts
+++ packages/paymaster-policy/src/index.ts
@@ -1,2 +1,4 @@
 export { evaluatePaymasterPolicy } from "./policy.js";
+export { buildPaymasterPolicySummary } from "./summary.js";
 export type { PaymasterPolicyConfig, PaymasterPolicyDecision } from "./types.js";
+export type { PaymasterPolicySummary } from "./summary.js";
