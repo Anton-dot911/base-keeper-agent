98784cc223fa0f498ee0c37eb27d3a31057ccd05
Anton-dot911 <132948024+Anton-dot911@users.noreply.github.com>
2026-05-14 20:01:56 +0300

    Export Hyperliquid paper trading modules

--- packages/hyperliquid-intel/src/index.ts
+++ packages/hyperliquid-intel/src/index.ts
@@ -1,13 +1,18 @@
 export { createHyperliquidInfoClient, HyperliquidInfoClient } from "./client.js";
 export { fetchRecentUserFills, fetchUserFillsByTime } from "./fills-reader.js";
+export { fetchAllMids } from "./market-data.js";
 export { normalizeHyperliquidFill, normalizeHyperliquidFills } from "./trade-normalizer.js";
 export { scoreWallet } from "./wallet-score.js";
 export { buildWalletIntelligenceReport } from "./report.js";
 export { buildHyperliquidWatchlistReport } from "./watchlist.js";
+export { buildPaperTradingReport } from "./paper-trading.js";
 export type {
   CoinPerformance,
   HyperliquidFill,
+  HyperliquidPaperTrade,
+  HyperliquidPaperTradingReport,
   HyperliquidSide,
+  HyperliquidTradeEvent,
   HyperliquidWatchlistReport,
   HyperliquidWatchlistSignal,
   NormalizedHyperliquidTrade,
